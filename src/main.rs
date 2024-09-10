6d5db3943f37a380262a23cf5b04ab34c4179d21
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2024-09-10 18:38:44 +0000

    Peephole optimization
    Replace `[-]` by a new set memory location to zero instruction and
    merge consequitive `+`, `-`, `>`, `<` into a single instruction.

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,20 @@ impl Default for Context<Stdin, Stdout> {
     }
 }
 
-struct Op<I: Read, O: Write> {
-    op: fn(arg: isize, context: &mut Context<I, O>, memory: *mut u8, dp: usize, program: *const Op<I, O>) -> Result<(), io::Error>,
+struct OpImpl<I: Read, O: Write> {
+    op: fn(arg: isize, context: &mut Context<I, O>, memory: *mut u8, dp: usize, program: *const OpImpl<I, O>) -> Result<(), io::Error>,
     arg: isize,
 }
 
-impl<I: Read, O: Write> Clone for Op<I, O> {
+impl<I: Read, O: Write> Clone for OpImpl<I, O> {
     fn clone(&self) -> Self {
         Self { op: self.op, arg: self.arg }
     }
 }
 
-fn dispatch<I: Read, O: Write>(context: &mut Context<I, O>, memory: *mut u8, dp: usize, program: *const Op<I, O>, offset: isize) -> Result<(), io::Error> {
+fn dispatch<I: Read, O: Write>(context: &mut Context<I, O>, memory: *mut u8, dp: usize, program: *const OpImpl<I, O>, offset: isize) -> Result<(), io::Error> {
     let program = unsafe { program.offset(offset) };
-    let op: &Op<I, O> = unsafe { &*program };
+    let op: &OpImpl<I, O> = unsafe { &*program };
 
     (op.op)(op.arg, context, memory, dp, program)
 }
@@ -40,19 +40,15 @@ fn out_of_bounds() -> Result<(), io::Error> {
     Err(io::Error::new(io::ErrorKind::InvalidInput, "out of bounds"))
 }
 
-fn op_unexpected_op<I: Read, O: Write>(_arg: isize, _context: &mut Context<I, O>, _memory:  *mut u8, _dp: usize, _program: *const Op<I, O>) -> Result<(), io::Error> {
-    unreachable!("unexpected op")
-}
-
-fn op_halt<I: Read, O: Write>(_arg: isize, _context: &mut Context<I, O>, _memory:  *mut u8, _dp: usize, _program: *const Op<I, O>) -> Result<(), io::Error> {
+fn op_halt<I: Read, O: Write>(_arg: isize, _context: &mut Context<I, O>, _memory:  *mut u8, _dp: usize, _program: *const OpImpl<I, O>) -> Result<(), io::Error> {
     Ok(())
 }
 
-fn op_inc_dp<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const Op<I, O>) -> Result<(), io::Error> {
+fn op_inc_dp<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const OpImpl<I, O>) -> Result<(), io::Error> {
     dispatch(context, memory, dp.wrapping_add_signed(arg), program, 1)
 }
 
-fn op_inc_memory<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const Op<I, O>) -> Result<(), io::Error> {
+fn op_inc_memory<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const OpImpl<I, O>) -> Result<(), io::Error> {
     if dp >= MEMORY_SIZE {
         return out_of_bounds();
     }
@@ -63,7 +59,18 @@ fn op_inc_memory<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, mem
     dispatch(context, memory, dp, program, 1)
 }
 
-fn op_jmp_zero<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const Op<I, O>) -> Result<(), io::Error> {
+fn op_set_zero<I: Read, O: Write>(_arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const OpImpl<I, O>) -> Result<(), io::Error> {
+    if dp >= MEMORY_SIZE {
+        return out_of_bounds();
+    }
+
+    let ptr: &mut u8 = unsafe { &mut *memory.add(dp) };
+    *ptr = 0;
+
+    dispatch(context, memory, dp, program, 1)
+}
+
+fn op_jmp_zero<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const OpImpl<I, O>) -> Result<(), io::Error> {
     if dp >= MEMORY_SIZE {
         return out_of_bounds();
     }
@@ -71,7 +78,7 @@ fn op_jmp_zero<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memor
     dispatch(context, memory, dp, program, if unsafe { *memory.add(dp) } == 0 { arg } else { 1 })
 }
 
-fn op_jmp_non_zero<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const Op<I, O>) -> Result<(), io::Error> {
+fn op_jmp_non_zero<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const OpImpl<I, O>) -> Result<(), io::Error> {
     if dp >= MEMORY_SIZE {
         return out_of_bounds();
     }
@@ -79,7 +86,7 @@ fn op_jmp_non_zero<I: Read, O: Write>(arg: isize, context: &mut Context<I, O>, m
     dispatch(context, memory, dp, program, if unsafe { *memory.add(dp) } != 0 { arg } else { 1 })
 }
 
-fn op_write<I: Read, O: Write>(_arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const Op<I, O>) -> Result<(), io::Error> {
+fn op_write<I: Read, O: Write>(_arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const OpImpl<I, O>) -> Result<(), io::Error> {
     if dp >= MEMORY_SIZE {
         return out_of_bounds();
     }
@@ -88,7 +95,7 @@ fn op_write<I: Read, O: Write>(_arg: isize, context: &mut Context<I, O>, memory:
     dispatch(context, memory, dp, program, 1)
 }
 
-fn op_read<I: Read, O: Write>(_arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const Op<I, O>) -> Result<(), io::Error> {
+fn op_read<I: Read, O: Write>(_arg: isize, context: &mut Context<I, O>, memory:  *mut u8, dp: usize, program: *const OpImpl<I, O>) -> Result<(), io::Error> {
     if dp >= MEMORY_SIZE {
         return out_of_bounds();
     }
@@ -109,35 +116,88 @@ fn op_read<I: Read, O: Write>(_arg: isize, context: &mut Context<I, O>, memory: 
     dispatch(context, memory, dp, program, 1)
 }
 
-struct Block<I: Read, O: Write> {
-    ops: Vec<Op<I, O>>,
+#[derive(Debug, PartialEq, Clone)]
+enum Op {
+    IncDp(isize),
+    IncMemory(isize),
+    SetZero,
+    Write,
+    Read,
 }
 
-impl<I: Read, O: Write> Block<I, O> {
-    fn new(program: &[u8]) -> Self {
-        let mut ops = Self::parse(program);
-        ops.push(Op { op: op_halt, arg: 0 });
+impl From<u8> for Op {
+    fn from(ch: u8) -> Self {
+        match ch {
+            b'<' => Op::IncDp(-1),
+            b'>' => Op::IncDp(1),
+            b'+' => Op::IncMemory(1),
+            b'-' => Op::IncMemory(-1),
+            b'.' => Op::Write,
+            b',' => Op::Read,
+            _ => unreachable!("unexpected op: {}", ch),
+        }
+    }
+}
 
-        Self { ops }
+impl Op {
+    fn to_impl<I: Read, O: Write>(self) -> OpImpl<I, O> {
+        match self {
+            Self::IncDp(arg) => OpImpl { op: op_inc_dp, arg },
+            Self::IncMemory(arg) => OpImpl { op: op_inc_memory, arg },
+            Self::SetZero => OpImpl { op: op_set_zero, arg: 0 },
+            Self::Write => OpImpl { op: op_write, arg: 0 },
+            Self::Read => OpImpl { op: op_read, arg: 0 },
+        }
     }
 
-    fn parse(program: &[u8]) -> Vec<Op<I, O>> {
-        let mut ops = vec! [];
+    fn try_merge(&mut self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::IncDp(ref mut a), Self::IncDp(b)) => {
+                *a += b;
+                true
+            },
+            (Self::IncMemory(ref mut a), Self::IncMemory(b)) => {
+                *a += b;
+                true
+            },
+            _ => false,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Parsed {
+    Block(Vec<Op>),
+    Loop(Vec<Parsed>),
+    Once(Vec<Parsed>),
+}
+
+impl From<&[u8]> for Parsed {
+    fn from(program: &[u8]) -> Self {
+        Self::Once(Parsed::parse(program)).rewrite()
+    }
+}
+
+impl Parsed {
+    fn parse(program: &[u8]) -> Vec<Parsed> {
+        let mut blocks = vec! [];
         let mut i = 0;
 
         while i < program.len() {
             match program[i] {
                 b'<' | b'>' | b'+' | b'-' | b'.' | b',' => {
-                    ops.push(Self::op_from_byte(program[i]));
+                    if let Some(Parsed::Block(ref mut last_block)) = blocks.last_mut() {
+                        last_block.push(Op::from(program[i]));
+                    } else {
+                        blocks.push(Parsed::Block(vec! [Op::from(program[i])]));
+                    }
                     i += 1;
                 },
                 b'[' => {
                     let ends_at = Self::loop_slice_at(&program[i + 1..]) + i;
-                    let sub_ops = Self::parse(&program[i + 1..ends_at]);
+                    let sub_blocks = Self::parse(&program[i + 1..ends_at]);
 
-                    ops.push(Op { op: op_jmp_zero, arg: (sub_ops.len() + 2) as isize });
-                    ops.extend_from_slice(&sub_ops);
-                    ops.push(Op { op: op_jmp_non_zero, arg: -(sub_ops.len() as isize) });
+                    blocks.push(Parsed::Loop(sub_blocks));
 
                     i = ends_at + 1;
                 },
@@ -146,19 +206,7 @@ impl<I: Read, O: Write> Block<I, O> {
             }
         }
 
-        ops
-    }
-
-    fn op_from_byte(byte: u8) -> Op<I, O> {
-        match byte {
-            b'<' => Op { op: op_inc_dp, arg: -1 },
-            b'>' => Op { op: op_inc_dp, arg: 1 },
-            b'+' => Op { op: op_inc_memory, arg: 1 },
-            b'-' => Op { op: op_inc_memory, arg: -1 },
-            b'.' => Op { op: op_write, arg: 0 },
-            b',' => Op { op: op_read, arg: 0 },
-            _ => Op { op: op_unexpected_op, arg: 0 },
-        }
+        blocks
     }
 
     fn loop_slice_at(program: &[u8]) -> usize {
@@ -177,9 +225,91 @@ impl<I: Read, O: Write> Block<I, O> {
 
         i
     }
+
+    fn is_single_op(&self, predicate: impl Fn(&Op) -> bool) -> bool {
+        match self {
+            Self::Block(ops) => ops.len() == 1 && predicate(&ops[0]),
+            _ => false,
+        }
+    }
+
+    fn merge_consecutive_ops(ops: &[Op]) -> Vec<Op> {
+        let mut merged: Vec<Op> = vec! [];
+
+        for op in ops {
+            if let Some(last_merged) = merged.last_mut() {
+                if !last_merged.try_merge(op) {
+                    merged.push(op.clone());
+                }
+            } else {
+                merged.push(op.clone());
+            }
+        }
+
+        merged
+    }
+
+    fn rewrite(self) -> Self {
+        match self {
+            Self::Block(ops) => Self::Block(Self::merge_consecutive_ops(&ops)),
+            Self::Loop(sub_blocks) => {
+                let new_sub_blocks = sub_blocks.into_iter().map(Self::rewrite).collect::<Vec<Parsed>>();
+
+                if new_sub_blocks.len() == 1 && (new_sub_blocks[0].is_single_op(|op| Op::SetZero.eq(op) || Op::IncMemory(-1).eq(op))) {
+                    Self::Block(vec! [Op::SetZero])
+                } else {
+                    Self::Loop(new_sub_blocks)
+                }
+            },
+            Self::Once(blocks) => Self::Once(blocks.into_iter().map(Self::rewrite).collect())
+        }
+    }
 }
 
-fn execute<I: Read, O: Write>(context: &mut Context<I, O>, block: &Block<I, O>)  -> Result<(), io::Error> {
+struct Compiled<I: Read, O: Write> {
+    ops: Vec<OpImpl<I, O>>,
+}
+
+impl<I: Read, O: Write> From<Parsed> for Compiled<I, O> {
+    fn from(parsed: Parsed) -> Self {
+        let mut ops = Self::compile(parsed);
+        ops.push(OpImpl { op: op_halt, arg: 0 });
+
+        Self { ops }
+    }
+}
+
+impl<I: Read, O: Write> Compiled<I, O> {
+    fn compile(parsed: Parsed) -> Vec<OpImpl<I, O>> {
+        let mut ops = vec! [];
+
+        match parsed {
+            Parsed::Block(block) => {
+                ops.extend(block.into_iter().map(Op::to_impl));
+            },
+            Parsed::Loop(sub_blocks) => {
+                let mut loop_ops = vec! [];
+
+                for sub_block in sub_blocks {
+                    loop_ops.extend(Self::compile(sub_block));
+                }
+
+                ops.push(OpImpl { op: op_jmp_zero, arg: loop_ops.len() as isize + 2 });
+                ops.extend_from_slice(&loop_ops);
+                ops.push(OpImpl { op: op_jmp_non_zero, arg: -(loop_ops.len() as isize) });
+            },
+            Parsed::Once(blocks) => {
+                for block in blocks {
+                    ops.extend(Self::compile(block));
+                }
+            },
+        }
+
+        ops
+    }
+}
+
+fn execute<I: Read, O: Write>(context: &mut Context<I, O>, block: &Compiled<I, O>)  -> Result<(), io::Error> {
     let mut memory = vec! [0; 30_000];
 
     dispatch(
@@ -194,7 +324,8 @@ fn execute<I: Read, O: Write>(context: &mut Context<I, O>, block: &Block<I, O>) 
 fn main() -> Result<(), io::Error> {
     for arg in std::env::args().skip(1) {
         let program = std::fs::read(&arg)?;
-        let block: Block<Stdin, Stdout> = Block::new(&program[..]);
+        let parsed = Parsed::from(&program[..]);
+        let block: Compiled<Stdin, Stdout> = Compiled::from(parsed);
 
         execute(&mut Context::default(), &block)?;
     }
@@ -217,9 +348,35 @@ mod tests {
     }
 
     #[test]
+    fn rewrite_consequitive_ops() {
+        let program = b"++>--";
+
+        assert_eq!(
+            Parsed::from(&program[..]),
+            Parsed::Once(vec! [
+                Parsed::Block(vec! [Op::IncMemory(2), Op::IncDp(1), Op::IncMemory(-2)])
+            ])
+        );
+    }
+
+    #[test]
+    fn rewrite_set_zero_loop() {
+        let program = b">[[+--]]";
+
+        assert_eq!(
+            Parsed::from(&program[..]),
+            Parsed::Once(vec! [
+                Parsed::Block(vec! [Op::IncDp(1)]),
+                Parsed::Block(vec! [Op::SetZero])
+            ])
+        );
+    }
+
+    #[test]
     fn hello_world() -> Result<(), io::Error> {
         let program = b">>+<--[[<++>->-->+++>+<<<]-->++++]<<.<<-.<<..+++.>.<<-.>.+++.------.>>-.<+.>>.";
-        let block = Block::new(&program[..]);
+        let parsed = Parsed::from(&program[..]);
+        let block = Compiled::from(parsed);
         let mut context = Context::with_input(b"");
 
         execute(&mut context, &block)?;
@@ -231,7 +388,8 @@ mod tests {
     #[test]
     fn quick_sort() -> Result<(), io::Error> {
         let program = b">>+>>>>>,[>+>>,]>+[--[+<<<-]<[<+>-]<[<[->[<<<+>>>>+<-]<<[>>+>[->]<<[<]<-]>]>>>+<[[-]<[>+<-]<]>[[>>>]+<<<-<[<<[<<<]>>+>[>>>]<-]<<[<<<]>[>>[>>>]<+<<[<<<]>-]]+<<<]+[->>>]>>]>>[.>>>]";
-        let block = Block::new(&program[..]);
+        let parsed = Parsed::from(&program[..]);
+        let block = Compiled::from(parsed);
         let mut context = Context::with_input(b"2635789014");
 
         execute(&mut context, &block)?;
